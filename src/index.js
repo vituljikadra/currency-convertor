01fa4b69cbec51b1f6987ac8f4551f29dbea53cf
vitul jikadra <vituljikadra9103@gmail.com>
2025-12-17 22:56:57 +0530

    Add Google AdSense meta tag to index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  <meta name="google-adsense-account" content="ca-pub-1171650683532229">
   </React.StrictMode>
 );
 
